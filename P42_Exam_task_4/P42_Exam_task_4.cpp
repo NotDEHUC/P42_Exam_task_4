4db9d30aa492eb14730d1515b1b600b33ebbf514
student <student@SSD1302>
2025-10-14 19:08:12 +0300

    Added print functions

--- P42_Exam_task_4/P42_Exam_task_4.cpp
+++ P42_Exam_task_4/P42_Exam_task_4.cpp
@@ -2,7 +2,132 @@
 
 using namespace std;
 
+void zeroFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                                |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl; 
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void oneFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                               ( )" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void twoFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                               ( )" << endl;
+    cout << " |                              _---_" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             \\_____/" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void threeFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                          _     |" << endl;
+    cout << " |                          \\\\   ( )" << endl;
+    cout << " |                           \\\\ _---_" << endl;
+    cout << " |                            \\|     |" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             \\_____/" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void fourFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                          _     |     _" << endl;
+    cout << " |                          \\\\   ( )   //" << endl;
+    cout << " |                           \\\\ _---_ //" << endl;
+    cout << " |                            \\|     |/" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             \\_____/" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void fiveFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                          _     |     _" << endl;
+    cout << " |                          \\\\   ( )   //" << endl;
+    cout << " |                           \\\\ _---_ //" << endl;
+    cout << " |                            \\|     |/" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             \\_____/" << endl;
+    cout << " |                             //" << endl;
+    cout << " |                            //" << endl;
+    cout << " |                           //" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
+void sixFails() {
+    cout << "-------------------------------------" << endl;
+    cout << " |                                |" << endl;
+    cout << " |                          _     |     _" << endl;
+    cout << " |                          \\\\   ( )   //"<< endl;
+    cout << " |                           \\\\ _---_ //" << endl;
+    cout << " |                            \\|     |/" << endl;
+    cout << " |                             |     |" << endl;
+    cout << " |                             \\_____/" << endl;
+    cout << " |                             //   \\\\" << endl;
+    cout << " |                            //     \\\\" << endl;
+    cout << " |                           //       \\\\" << endl;
+    cout << " |" << endl;
+    cout << " |" << endl;
+    cout << "-------------------------------------" << endl;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    zeroFails();
+    oneFails();
+    twoFails();
+    threeFails();
+    fourFails();
+    fiveFails();
+    sixFails();
 }
