e38551fd46f5d3ddc05a39f4a57be14ee8022abf
student <student@SSD2103>
2025-10-18 14:48:45 +0300

    Need a word randomizer

--- P42_Exam_task_4/P42_Exam_task_4.cpp
+++ P42_Exam_task_4/P42_Exam_task_4.cpp
@@ -121,34 +121,90 @@ void sixFails() {
     cout << "-------------------------------------" << endl;
 }
 
-bool isContainLetter(char letter, char* word) {
+void showWord(char* arr, const char* word) {
+    for (size_t i = 0; i < strlen(word); i++)
+    {
+        if(arr[i] == word[i]) {
+            cout << arr[i] << " ";
+        }
+        else
+        {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+bool isContainLetter(char letter, const char* word, char* arr) {
     for (size_t i = 0; i < strlen(word) + 1; i++)
     {
-        if (letter == word[i]) {
+        if (letter == word[i] && letter != arr[i]) {
+            arr[i] = letter;
             return true;
         }
     }
     return false;
 }
 
+bool isWin(char* arr, const char* word) {
+    for (size_t i = 0; i < strlen(word); i++)
+    {
+        if (arr[i] != word[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
-    const char* word = "hydrogen";
+    const char* word = "word";
     char letter;
     int fail = 0;
+
+    char* arr = new char[strlen(word)];
+
     while (true)
     {
-        if (fail == 0) {
-
+        if (isWin(arr, word)) {
+            cout << "Congratulations!!!";
+            return false;
         }
+
+        if (fail == 0) {
+            zeroFails();
+        }
+        else if (fail == 1) {
+            oneFails();
+        }
+        else if (fail == 2) {
+            twoFails();
+        }
+        else if (fail == 3) {
+            threeFails();
+        }
+        else if (fail == 4) {
+            fourFails();
+        }
+        else if (fail == 5) {
+            fiveFails();
+        }
+        else if (fail == 6) {
+            cout << "Game over!:(" << endl;
+        }
+
+        showWord(arr, word);
+
         cout << "Your letter: ";
         cin >> letter;
-        if (isContainLetter) {
 
+        if (isContainLetter(letter, word, arr)) {
+            continue;
         }
         else
         {
             fail++;
         }
     }
+
+    //create word randomizer
 }
